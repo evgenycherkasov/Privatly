1e9bbe3cd25587af55534817f649b2f0f61f916e
Evgeny Cherkasov <evgenycherkasov1998@gmail.com>
2023-03-12 11:38:38 +0700

    feat(middleware): добавить фильтр для фильтрации ip адресов

---
+++ src/API/Privatly.API/Middlewares/ClientIpCheckActionFilter.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Privatly.API.Middlewares;
+
+public class ClientIpCheckActionFilter : ActionFilterAttribute
+{
+    private readonly ILogger _logger;
+    private readonly string _safeList;
+
+    public ClientIpCheckActionFilter(string safeList, ILogger logger)
+    {
+        _safeList = safeList;
+        _logger = logger;
+    }
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var remoteIp = context.HttpContext.Connection.RemoteIpAddress;
+        
+        _logger.LogDebug("Remote IpAddress: {RemoteIp}", remoteIp);
+        var ip = _safeList.Split(';');
+
+        if (remoteIp.IsIPv4MappedToIPv6)
+        {
+            remoteIp = remoteIp.MapToIPv4();
+        }
+
+        var badIp = !ip.Select(IPAddress.Parse).Contains(remoteIp);
+
+        if (badIp)
+        {
+            _logger.LogWarning("Forbidden Request from IP: {RemoteIp}", remoteIp);
+            context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+            return;
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
